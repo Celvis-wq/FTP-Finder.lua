f3c874bb47c3b2a2f073766e7284315176f1cef6
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-11-16 10:37:25 -0600

    Update FTPFinder.lua

--- FTPFinder.lua
+++ FTPFinder.lua
@@ -1,26 +1,37 @@
--- FTP Finder.lua
 --[[
-    Version: 0.0.3
+    FTP Finder.lua
+    Version: 0.0.4
     Created by: Celvis#5477
 ]]
 
 local socket = require("socket")
 local http = require("socket.http")
 
+-- Constants
+local portsUrl = "https://raw.githubusercontent.com/Celvis-wq/ports/main/ports.txt"
+local defaultTimeout = 5
+
 -- Cache DNS resolution
 local ip = io.read()
+if not ip or ip == "" then
+    error("Invalid IP address.")
+end
+
 local ipInfo = socket.dns.getaddrinfo(ip)
-local resolvedIp = ipInfo[1].addr
+local resolvedIp = ipInfo and ipInfo[1] and ipInfo[1].addr or error("DNS resolution failed for IP: " .. ip)
 
 -- Increase timeout
-local timeout = 5
+local timeout = defaultTimeout
 
 -- Function to check if a port is open
 local function isPortOpen(port)
     local sock = socket.tcp()
     sock:settimeout(timeout)
     local result, errorMsg = sock:connect(resolvedIp, port)
-    sock:close()
+    local closeStatus, closeError = sock:close()
+    if not closeStatus then
+        -- Handle close error if necessary
+    end
     return result ~= nil
 end
 
@@ -28,7 +39,7 @@ end
 local function loadFtpPorts(url)
     local response, statusCode = http.request(url)
     if statusCode ~= 200 then
-        error("Failed to load ports from URL. HTTP status code: " .. tostring(statusCode))
+        error("Failed to load ports from URL (" .. url .. "). HTTP status code: " .. tostring(statusCode))
     end
     
     local ports = {}
@@ -52,7 +63,6 @@ end
 
 -- Main
 local function main()
-    local portsUrl = "https://raw.githubusercontent.com/Celvis-wq/ports/main/ports.txt"
     local ftpPorts = loadFtpPorts(portsUrl)
     
     print("Scanning FTP ports on " .. ip)
